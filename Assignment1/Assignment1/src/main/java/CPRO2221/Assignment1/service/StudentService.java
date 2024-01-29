9808b846d9d2116bfe9838f63fb5c31f321c1767
kriostt <129346100+kriostt@users.noreply.github.com>
2024-01-28 18:46:21 -0700

    Update StudentService.java

--- Assignment1/Assignment1/src/main/java/CPRO2221/Assignment1/service/StudentService.java
+++ Assignment1/Assignment1/src/main/java/CPRO2221/Assignment1/service/StudentService.java
@@ -1,4 +1,40 @@
 package CPRO2221.Assignment1.service;
 
+import CPRO2221.Assignment1.entity.Student;
+import CPRO2221.Assignment1.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+    // Annotation automatically injects instance of StudentRepository
+    @Autowired
+    private StudentRepository studentRepository;
+
+    // Save student by calling the save() function from StudentRepository
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    // Get all students by calling the getAllStudents() function from StudentRepository
+    public List<Student> getAllStudents() {
+        return studentRepository.getAllStudents();
+    }
+
+    // Get specific student by calling the findById() function from StudentRepository
+    public Student findById(int id) {
+        return studentRepository.findById(id);
+    }
+
+    // Update student by calling the update() function from StudentRepository
+    public Student updateStudent(int id, Student student) {
+        return studentRepository.update(id, student);
+    }
+
+    // Delete student by calling the delete() function from StudentRepository
+    public String deleteStudent(int id) {
+        return studentRepository.delete(id);
+    }
 }
