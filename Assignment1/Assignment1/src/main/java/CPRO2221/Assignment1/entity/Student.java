fd06edd428a0e852ff04a58bcf2c16ce67b59c76
kriostt <129346100+kriostt@users.noreply.github.com>
2024-01-28 17:53:07 -0700

    Create Student entity class

--- Assignment1/Assignment1/src/main/java/CPRO2221/Assignment1/entity/Student.java
+++ Assignment1/Assignment1/src/main/java/CPRO2221/Assignment1/entity/Student.java
@@ -1,4 +1,15 @@
 package CPRO2221.Assignment1.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Lombok annotations to automatically generate code for getters, setters, and constructors
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+    private int id;
+    private String name;
+    private String major;
 }
