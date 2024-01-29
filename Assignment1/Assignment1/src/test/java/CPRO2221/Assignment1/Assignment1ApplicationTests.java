bfcd6992f5af7590c3ad315bddc77991c91f4c33
kriostt <129346100+kriostt@users.noreply.github.com>
2024-01-28 17:39:20 -0700

    Create project

---
+++ Assignment1/Assignment1/src/test/java/CPRO2221/Assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package CPRO2221.Assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
