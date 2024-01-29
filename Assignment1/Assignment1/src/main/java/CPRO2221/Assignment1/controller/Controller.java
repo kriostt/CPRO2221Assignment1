3db06a16447030ecbf40226b479e0c5eef49517b
kriostt <129346100+kriostt@users.noreply.github.com>
2024-01-28 19:10:32 -0700

    Update Controller.java

--- Assignment1/Assignment1/src/main/java/CPRO2221/Assignment1/controller/Controller.java
+++ Assignment1/Assignment1/src/main/java/CPRO2221/Assignment1/controller/Controller.java
@@ -1,4 +1,46 @@
 package CPRO2221.Assignment1.controller;
 
+import CPRO2221.Assignment1.entity.Student;
+import CPRO2221.Assignment1.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
 public class Controller {
+    // Annotation automatically injects instance of StudentService
+    @Autowired
+    private StudentService studentService;
+
+    // Create a new student
+    @PostMapping("/create")
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    // Retrieve all students
+    @GetMapping("/all")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    // Retrieve a specific student by ID
+    @GetMapping("/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        return studentService.findById(studentId);
+    }
+
+    // Update an existing student
+    @PutMapping("/update/{studentId}")
+    public Student updateStudent(@PathVariable int studentId, @RequestBody Student student) {
+        return studentService.updateStudent(studentId, student);
+    }
+
+    // Delete a student by ID
+    @DeleteMapping("/delete/{studentId}")
+    public String deleteStudent(@PathVariable int studentId) {
+        return studentService.deleteStudent(studentId);
+    }
 }
